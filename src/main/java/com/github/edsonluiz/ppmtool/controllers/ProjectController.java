82b8fef417938a89bd2a37dc003185c030abca03
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-31 17:35:47 -0300

    Refactor json return of Validation error
    When store request has incorrect fields a JSON response should be send with fields and informative message.

--- src/main/java/com/github/edsonluiz/ppmtool/controllers/ProjectController.java
+++ src/main/java/com/github/edsonluiz/ppmtool/controllers/ProjectController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -22,7 +27,13 @@ public class ProjectController {
     @PostMapping()
     public ResponseEntity<?> store(@Valid @RequestBody Project project, BindingResult result) {
         if(result.hasErrors()) {
-            return new ResponseEntity<>("Invalid Project data", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
         Project returnOfSaveOperation = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(returnOfSaveOperation, HttpStatus.CREATED);
