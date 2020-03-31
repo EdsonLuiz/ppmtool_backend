0173bc9736f24d279c66db7db8f79dd4f6ddf974
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-31 16:55:08 -0300

    Validate project data from request

--- src/main/java/com/github/edsonluiz/ppmtool/controllers/ProjectController.java
+++ src/main/java/com/github/edsonluiz/ppmtool/controllers/ProjectController.java
@@ -5,10 +5,13 @@ import com.github.edsonluiz.ppmtool.models.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -17,7 +20,10 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping()
-    public ResponseEntity<Project> store(@RequestBody Project project) {
+    public ResponseEntity<?> store(@Valid @RequestBody Project project, BindingResult result) {
+        if(result.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project data", HttpStatus.BAD_REQUEST);
+        }
         Project returnOfSaveOperation = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(returnOfSaveOperation, HttpStatus.CREATED);
     }
--- src/main/java/com/github/edsonluiz/ppmtool/models/entities/Project.java
+++ src/main/java/com/github/edsonluiz/ppmtool/models/entities/Project.java
@@ -1,6 +1,10 @@
 package com.github.edsonluiz.ppmtool.models.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_project;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
